5872d0ba51a7e4c2563a1e5276f93b2a85f578b2
student account <student@debian-BULLSEYE-live-builder-AMD64>
2026-02-13 15:08:09 -0600

    Task: Create Splash Screen

---
+++ splash_screen.py
@@ -0,0 +1,29 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+# create splash screen object
+splash_root = Tk()
+splash_root.title("Photon")
+splash_root.geometry("1200x800")
+splash_root.resizable(False, False)
+
+# resize splash screen image to match the window's size
+image = Image.open("logo.png")
+resized_img = image.resize((1200, 800))
+img = ImageTk.PhotoImage(resized_img)
+
+splash_label = Label(splash_root, image = img)
+splash_label.image = img
+splash_label.pack()
+
+def main():
+    # close splash screen
+    splash_root.destroy()
+
+    # call function to open player entry screen
+    # function_call()
+
+# display splash screen for 5 seconds, then call main
+splash_root.after(5000, main)
+
+mainloop()
